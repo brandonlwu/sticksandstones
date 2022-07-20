97dfbdd2fc4dc2f70c5cc64741ad73b3d58a26aa
Brandon Wu <brandonlwu@Brandons-MacBook-Pro-2.local>
2022-07-20 16:32:44 -0400

    more flows

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -1,19 +1,30 @@
 import '../App.css';
 import maindog from '../assets/dog.png'
+import Stick from '../components/Stick'
+import React, { useState } from 'react';
 
 const stickRankings = []
+const stickImages = []
 for (let i = 0; i < 10; i++) {
     stickRankings.push(Math.floor(Math.random() * 100));
+    stickImages.push(Math.floor(Math.random() * 14));
 }
 console.log(stickRankings)
 
 function GamePage() {
+  const [stick, updateStick] = useState(0)
+
+  function updateGameState() {
+    if (stick < 9)
+    updateStick(stick+1)
+  }
+
   return (
     <div className="container">
       <div className = 'imgcontainer'>
-        <p className = 'title'>Sticks and Bones</p>
-        <img src = {maindog} className = 'mainimg'/>
+        <Stick rating = {stickRankings[stick]} stick = {stickImages[stick]}/>
         <p>{stickRankings}</p>
+        <button onClick = {()=> updateGameState()}>Next</button>
       </div>
     </div>
   );
--- src/pages/RulesPage.js
+++ src/pages/RulesPage.js
@@ -9,15 +9,15 @@ function RulesPage() {
     <div className="container">
         <p className = 'title'>Here are the rules</p>
         <div className = 'flexbox'>
-            <Stick rating = {93} stick = {1}/>
+            <Stick rating = {78} stick = {1}/>
             <div>
                 <p className = 'rules'>As you walk on this trail with Albie, you will encounter 10 sticks.</p>
                 <ul>
-                  <li className='rules bullet'>The goal of the game is the find the best stick possible for Albie</li>
+                  <li className='rules bullet'>The goal of the game is the find the best stick possible for Albie.</li>
                   <li className='rules bullet'>The stick's quality is determined by Albie's rating of the stick.</li>
                   <li className='rules bullet'>The higher the rating, the better the stick.</li>
                   <li className='rules bullet'>With each stick, you have the choice of keeping it or passing on.</li>
-                  <li className='rules bullet'>Once you choose a stick to keep, the game is over.</li>
+                  <li className='rules bullet'>Once you choose to keep a stick, the game is over.</li>
                   <li className='rules bullet'>Once you pass a stick, you can't go back.</li>
                   <li className='rules bullet'>The quality of each stick is random.</li>
                 </ul>
